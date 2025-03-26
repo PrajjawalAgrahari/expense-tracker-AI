3227e77a4a9f7c6dcac8bc7ea82a599bb2cab52f
Prajjawal <agrahariprajjawal5@gmail.com>
2025-03-26 17:47:18 +0530

    Added header and set up ingest and arcjet

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,21 +1,54 @@
 import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/expensify-seeklogo.png";
+import { PenBox, LayoutDashboard } from "lucide-react";
 
 export default function Header() {
     return (
-        <header className="flex justify-end items-center p-4 gap-4 h-16 fixed top-0">
-            <SignedOut>
-                <SignInButton />
-                <SignUpButton /> 
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
+        <header className="flex items-center h-20 bg-white border-b border-gray-200">
+            <nav className="flex justify-between w-full items-center px-6">
+                <Link href="/">
+                    <Image src={Logo} alt="expensify logo" className="h-12 w-40"></Image>
+                </Link>
+                <div className="flex gap-4 items-center">
+                    <SignedOut>
+                        <SignInButton forceRedirectUrl="/dashboard">
+                            <Button variant={"outline"} className="cursor-pointer">
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <Link href="/dashboard">
+                            <Button variant={"outline"}>
+                                <LayoutDashboard size={18} />
+                                <span>Dashboard</span>
+                            </Button>
+                        </Link>
+                        <Link href="/transaction/create">
+                            <Button>
+                                <PenBox size={18} />
+                                <span>Add Transaction</span>
+                            </Button>
+                        </Link>
+                        <UserButton
+                            appearance={{
+                                elements: {
+                                    avatarBox: 'w-15 h-15'
+                                }
+                            }}
+                        />
+                    </SignedIn>
+                </div>
+            </nav>
         </header>
-    )
+    );
 }
