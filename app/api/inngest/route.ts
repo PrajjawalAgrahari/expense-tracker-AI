0b57804727e283401ec30df3c600485c95f2d7b5
Prajjawal <agrahariprajjawal5@gmail.com>
2025-04-14 00:41:14 +0530

    Implemented the cron job for recurring transaction

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,13 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { sendBudgetAlerts } from "../../../inngest/functions";
+import { triggerRecurringTransaction, process } from "../../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     sendBudgetAlerts, 
+    triggerRecurringTransaction,
+    process
   ],
 });
--- inngest/functions.ts
+++ inngest/functions.ts
@@ -2,6 +2,84 @@ import { inngest } from "./client";
 import { prisma } from "@/app/lib/prisma";
 import { sendEmail } from "@/app/lib/send-email";
 
+export const triggerRecurringTransaction = inngest.createFunction(
+    { id: "Trigger-Recurring-Transaction" },
+    { cron: "0 0 * * *" },
+    async ({ step }) => {
+        const startOfToday = new Date();
+        startOfToday.setHours(0, 0, 0, 0);
+        startOfToday.setMinutes(startOfToday.getMinutes() + 330);
+
+        const endOfToday = new Date();
+        endOfToday.setHours(23, 59, 59, 999);
+        endOfToday.setMinutes(endOfToday.getMinutes() + 330);
+
+        const recurringTransactions = await step.run("get-recurring-transactions", async () => {
+            return await prisma.transaction.findMany({
+                where: {
+                    isRecurring: true,
+                    nextRecurringDate: {
+                        gte: startOfToday,
+                        lte: endOfToday,
+                    },
+                    recurringInterval: {
+                        not: null,
+                    }
+                },
+            });
+        })
+
+        const events = recurringTransactions.map((transaction) => {
+            return {
+                name: "app/user.transaction.created",
+                data: {
+                    userId: transaction.userId,
+                    transaction,
+                },
+            };
+        });
+        await inngest.send(events);
+    }
+)
+
+export const process = inngest.createFunction(
+    {
+        id: "Handle-Recurring-Transaction",
+        name: "Handle Recurring Transaction",
+        throttle: {
+            limit: 10,
+            period: "1m",
+            key: "event.data.userId",
+        }
+    },
+    { event: "app/user.transaction.created" },
+    async ({ event, step }) => {
+        await step.run(`update-transaction-${event.data.transaction.id}`, async () => {
+            if (event.data.transaction.recurringInterval && event.data.transaction.nextRecurringDate) {
+                await prisma.$transaction([
+                    prisma.transaction.update({
+                        where: { id: event.data.transaction.id },
+                        data: {
+                            nextRecurringDate: computeNextRecurringDate(
+                                event.data.transaction.recurringInterval,
+                                new Date(event.data.transaction.nextRecurringDate)
+                            ),
+                        },
+                    }),
+                    prisma.account.update({
+                        where: { id: event.data.transaction.accountId },
+                        data: {
+                            balance: {
+                                decrement: event.data.transaction.type === "EXPENSE" ? parseFloat(event.data.transaction.amount) : -parseFloat(event.data.transaction.amount),
+                            },
+                        },
+                    }),
+                ]);
+            }
+        })
+    }
+)
+
 export const sendBudgetAlerts = inngest.createFunction(
     { id: "Check Budget Alerts" },
     { cron: "0 */6 * * *" },
@@ -61,6 +139,27 @@ export const sendBudgetAlerts = inngest.createFunction(
 const isNewMonth = (lastAlertSent: Date) => {
     const currentDate = new Date();
     return lastAlertSent.getMonth() !== currentDate.getMonth() || lastAlertSent.getFullYear() !== currentDate.getFullYear();
+}
+
+function computeNextRecurringDate(recurringInterval: string, date: Date) {
+    const nextDate = new Date(date);
+    switch (recurringInterval) {
+        case "DAILY":
+            nextDate.setDate(nextDate.getDate() + 1);
+            break;
+        case "WEEKLY":
+            nextDate.setDate(nextDate.getDate() + 7);
+            break;
+        case "MONTHLY":
+            nextDate.setMonth(nextDate.getMonth() + 1);
+            break;
+        case "YEARLY":
+            nextDate.setFullYear(nextDate.getFullYear() + 1);
+            break;
+        default:
+            throw new Error("Invalid recurring interval");
+    }
+    return nextDate;
 }
 
 // 'You can only send testing emails to your own email address (agrahariprajjawal5@gmail.com). To send emails to other recipients, please verify a domain at resend.com/domains, and change the `from` address to an email using this domain.',
