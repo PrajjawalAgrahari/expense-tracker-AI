136fd70f97228d752afdced9bb248e1129d524a9
Prajjawal <agrahariprajjawal5@gmail.com>
2025-04-16 23:09:26 +0530

    Added arcjet bot protection

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,26 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/account(.*)', '/transaction(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
+const aj = arcjet({
+    key: process.env.ARCJET_KEY!,
+    rules: [
+        shield({
+            mode: "LIVE"
+        }),
+        detectBot({
+            mode: "LIVE",
+            allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+        })
+    ]
+})
+
+const clerk = clerkMiddleware(async (auth, req) => {
     if (isProtectedRoute(req)) await auth.protect()
 })
+
+export default createMiddleware(aj, clerk)
 
 export const config = {
     matcher: [
